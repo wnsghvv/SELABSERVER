71a30936083914ed25eb37141eb90167bac1947c
wnsghvv <54825899+wnsghvv@users.noreply.github.com>
2019-11-19 22:46:28 +0900

    Create GpsInfo.java

---
+++ last/GpsInfo.java
@@ -0,0 +1,13 @@
+package com.selab;
+
+public class GpsInfo
+{
+    public double longitude;
+    public double latitude;
+
+    public GpsInfo(double longitude, double latitude)
+    {
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+}
