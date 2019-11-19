6a902dcede123eaf4e5cf98fac412c038d7e1486
wnsghvv <54825899+wnsghvv@users.noreply.github.com>
2019-11-19 22:46:44 +0900

    Create Constants.java

---
+++ last/Constants.java
@@ -0,0 +1,27 @@
+package com.selab;
+
+public class Constants
+{
+    public final static int INPUT_BUFFER_SIZE = 1024;
+    public final static int IMAGE_INPUT_BUFFER_SIZE = 100000000;
+
+    public final static String SENDER = "sender";
+
+    public final static String PHONE = "phone";
+    public final static String DEVICE = "device";
+    public final static String NOTIFY = "notify";
+    public final static String CHECK = "check";
+    public final static String COMMAND = "cmd";
+    public final static String GET_GPS = "get_gps";
+    public final static String CHECK_GPS = "check_gps";
+    public final static String GPS_NOT_YET = "gps_not_yet";
+    public final static String GET_PIC = "get_pic";
+    public final static String CHECK_PIC = "check_pic";
+    public final static String PIC_NOT_YET = "pic_not_yet";
+
+    public final static String NO_COMMAND = "no";
+    public final static String WAIT = "wait";
+
+    public final static String LATITUDE = "lat";
+    public final static String LONGITUDE = "lon";
+}
