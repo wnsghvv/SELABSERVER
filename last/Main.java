7ece34e97e3b37881f7b30addb98bfb10b773637
wnsghvv <54825899+wnsghvv@users.noreply.github.com>
2019-11-19 22:46:05 +0900

    Create Main.java

---
+++ last/Main.java
@@ -0,0 +1,16 @@
+package com.selab;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try
+        {
+            var server = new ServerManager(4107);
+            server.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
